118ff932cc012e6cdd6dc96a07419079676e7244
Rohit Pawar <rohitpawar5062@gmail.com>
2024-11-04 00:24:16 +0530

    dfff

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,7 +5,7 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Socials = () => {
   return (
-    <div className='flex flex-col md:flex-row items-center justify-around p-4 rounded-t-lg shadow-lg'>
+    <div className='flex  items-center justify-around p-4 rounded-t-lg shadow-lg'>
       <a href="https://x.com/Rohit71081" className='flex flex-col items-center space-y-1 transition-transform transform hover:scale-105 hover:shadow-2xl duration-300 ease-in-out'>
         <div className='flex items-center justify-center bg-black text-white text-3xl md:text-4xl rounded-3xl shadow-lg w-20 md:w-24 h-20 md:h-24 hover:shadow-2xl transition-shadow duration-300'>
           <FaXTwitter />
