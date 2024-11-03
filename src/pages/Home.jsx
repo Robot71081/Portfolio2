1d0c6ea8fbe9fa2172e95f7615d8eba17935ea89
Rohit Pawar <rohitpawar5062@gmail.com>
2024-11-04 00:18:50 +0530

    ffg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,24 @@
-import React from 'react'
-import Statusbar from '../components/Statusbar'
-import Widgets from '../components/Widgets'
-import Socials from '../components/Socials'
-import Info from '../components/Info'
-import Projects from '../components/Projects'
+import React from 'react';
+import Statusbar from '../components/Statusbar';
+import Widgets from '../components/Widgets';
+import Socials from '../components/Socials';
+import Info from '../components/Info';
+import Projects from '../components/Projects';
 
 const Home = () => {
   return (
-    <div className='flex flex-row h-screen bg-cover bg-center  '>
-    <div className="hidden md:flex flex-1 bg-black">1st</div>
-    <div className="flex-1 ">
-      <Statusbar />
-      <Widgets />
-      <Info />
-      <Projects />
-      <Socials/>
+    <div className='flex flex-col md:flex-row h-screen bg-cover bg-center'>
+      <div className="hidden md:flex flex-1 bg-black">1st</div>
+      <div className="flex-1 overflow-auto p-4">
+        <Statusbar />
+        <Widgets />
+        <Info />
+        <Projects />
+        <Socials />
+      </div>
+      <div className="hidden md:flex flex-1 bg-black">3rd</div>
     </div>
-    <div className="hidden md:flex flex-1 bg-black">3rd</div>
-  </div>
-  
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
